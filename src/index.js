d501147c9c228c30f07b762a37f1d93ee18c4fa2
dymitrbaranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2025-07-25 18:42:48 +0300

    fox3

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import react from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
-// import "./styles/index.css";
-// import "./styles/variables.css";
+
 import App from "./components/App/App";
 import { Provider } from "react-redux";
 import store from "./features/store";
+
+import "./styles/reset.css";
+import "./styles/common.css";
 
 createRoot(document.getElementById("root")).render(
   <react.StrictMode>
