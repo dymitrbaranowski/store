560b06761e65212b64afdbd587250b69567386af
Dymitr Baranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2025-07-25 20:36:20 +0300

    Update store.js

--- src/features/store.js
+++ src/features/store.js
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import categoriesSlice from "./categories/categoriesSlice"; // Import the categories slice reducer
 
 export const store = configureStore({
   reducer: {
     // Add your reducers here
-    categories: "categoriesSlice", // Import and use the categories slice reducer
+    categories: categoriesSlice, // Import and use the categories slice reducer
     // categories: (state = [], action) => state, // Example placeholder reducer
     // products: (state = [], action) => state, // Example placeholder reducer
     // cart: (state = [], action) => state, // Example placeholder reducer
